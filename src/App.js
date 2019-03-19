ecb0140b3ddf6e9133d8385729a6ca14312a8e00
amir <kmuc72@gmail.com>
2019-03-19 20:01:55 +0100

    failed attempt at creating a new item

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import todosData from "./todosData";
 import TodoItem from "./todoItem";
+import TodoItemEmpty from "./todoItemEmpty";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: todosData
+      todos: todosData,
+      new: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeText = this.handleChangeText.bind(this);
   }
 
   handleChange(id) {
+    console.log("yay" + id);
     this.setState(prevState => {
       prevState.todos.map(item => {
         if (item.id === id) {
@@ -24,10 +28,24 @@ export default class App extends Component {
     });
   }
 
+  handleChangeText(event) {
+    console.log("here:" + event);
+
+    // const target = event.target;
+    // console.log(target.value);
+  }
+
   render() {
     var todoItems = this.state.todos.map(item => (
       <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
+    todoItems.push(
+      <TodoItemEmpty
+        handleChange={this.handleChangeText}
+        key={todoItems.length + 1}
+        key2={todoItems.length + 1}
+      />
+    );
     return <div className="todo-list">{todoItems}</div>;
   }
 }
---
+++ src/todoItemEmpty.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class TodoItemEmpty extends Component {
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#A9A9A9",
+      textDecoration: "line-through"
+    };
+
+    return (
+      <div className="todo-item">
+        <input
+          type="checkbox"
+          id="click"
+          //   checked={this.props.item.completed}
+          //   onChange={() => this.props.handleChange(this.props.item.id)}
+        />
+        <input
+          id="click"
+          onChange={() => this.props.handleChange(this.props.event)}
+          //   style={this.props.item.completed ? completedStyle : null}
+        >
+          {/* {this.props.text} */}
+        </input>
+      </div>
+    );
+  }
+}
